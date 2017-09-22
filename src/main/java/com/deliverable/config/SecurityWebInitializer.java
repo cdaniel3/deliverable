d61a3231131320fef2146941a300264f0ba22b97
cdaniel <cory@example.com>
2017-09-22 15:48:35 -0500

    initial commit for spring security

---
+++ src/main/java/com/deliverable/config/SecurityWebInitializer.java
@@ -0,0 +1,7 @@
+package com.deliverable.config;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebInitializer extends AbstractSecurityWebApplicationInitializer {
+
+}
