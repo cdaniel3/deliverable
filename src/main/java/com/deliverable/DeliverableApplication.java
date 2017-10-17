1e078d2b6b41c72ab141e89a69fc539d502f41d0
cdaniel <cory@example.com>
2017-10-17 14:21:39 -0500

    Initial deliverable + spring boot commit

---
+++ src/main/java/com/deliverable/DeliverableApplication.java
@@ -0,0 +1,32 @@
+package com.deliverable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+
+@SpringBootApplication
+public class DeliverableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeliverableApplication.class, args);
+	}
+	
+	@Bean
+	public CommandLineRunner CommandLineRunner(ApplicationContext ctx) {
+		return args -> {
+			System.out.println("DemoApplication !!!!!!!!!!!!!!!!!!");
+			System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+            String[] beanNames = ctx.getBeanDefinitionNames();
+            Arrays.sort(beanNames);
+            for (String beanName : beanNames) {
+                System.out.println(beanName);
+            }
+		};
+	}
+}
